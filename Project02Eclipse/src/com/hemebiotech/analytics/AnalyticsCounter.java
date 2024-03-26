bbeb6f03cb9ab62a5cb8c9b7c796fb579d67b04b
Voyvode <voyvode@yandex.com>
2024-03-26 21:06:11 +0100

    Correct typos on symptoms

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -35,7 +35,7 @@ public class AnalyticsCounter {
 		FileWriter writer = new FileWriter ("result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+		writer.write("dilated pupils: " + pupilCount + "\n");
 		writer.close();
 	}
 }
