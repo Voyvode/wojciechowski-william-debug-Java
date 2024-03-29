fcbc87b56a2fed5a0197525e73b7a3f58d82f000
Voyvode <voyvode@yandex.com>
2024-03-30 00:32:24 +0100

    Add try-with-resources blocks
    Surround BufferedReader and FileWriter with try-with-resources blocks to
    autoclose file accesses while separating each operation and catching
    related exceptions.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,40 +1,43 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;
 	private static int rashCount = 0;
 	private static int pupilCount = 0;
 	
-	public static void main(String args[]) throws Exception {
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+	public static void main(String args[]) {
+		try (var reader = new BufferedReader(new FileReader("symptoms.txt"))) {
+			String line = reader.readLine();
 
-		int i = 0;
-		while (line != null) {
-			i++;
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-				System.out.println("number of headaches: " + headacheCount);
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.equals("dilated pupils")) {
-				pupilCount++;
-			}
+			int i = 0;
+			while (line != null) {
+				i++;
+				System.out.println("symptom from file: " + line);
+				if (line.equals("headache")) {
+					headacheCount++;
+					System.out.println("number of headaches: " + headacheCount);
+				} else if (line.equals("rash")) {
+					rashCount++;
+				} else if (line.equals("dilated pupils")) {
+					pupilCount++;
+				}
 
-			line = reader.readLine();
+				line = reader.readLine();
+			}
+		} catch (FileNotFoundException e) {
+			System.err.println("Can’t read " + e.getMessage());
+		} catch (IOException e) {
+			System.err.println("IO error while reading: " + e.getMessage());
 		}
 
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dilated pupils: " + pupilCount + "\n");
-		writer.close();
+		try (var writer = new FileWriter("result.out")) {
+			writer.write("headache: " + headacheCount + "\n");
+			writer.write("rash: " + rashCount + "\n");
+			writer.write("dilated pupils: " + pupilCount + "\n");
+		} catch (IOException e) {
+			System.err.println("Can’t write to " + e.getMessage());
+		}
 	}
 }
